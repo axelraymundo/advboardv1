c567446651af04f4bcf2713a615adecc1f74cfeb
Axel Raymundo <stephen.raymundo@gmail.com>
2024-03-12 16:48:31 +0800

    limit update user

--- src/api/user/controllers/user.js
+++ src/api/user/controllers/user.js
@@ -10,18 +10,25 @@ module.exports = {
       "plugin::users-permissions.user",
       ctx.state.user.id,
       {
-        fields: ["id", "full_name", "nickname", "dm_name"],
+        fields: ["id", "full_name", "nickname", "dm_name", "user_type"],
       }
     );
     return entry;
   },
   async updateMe(ctx) {
+    let data = ctx.request.body;
+
+    //allowed data
+    Object.entries(data).forEach(([key, value]) => {
+      if (!["full_name", "nickname", "dm_name"].includes(key)) delete data[key];
+    });
+
     const entry = await strapi.entityService.update(
       "plugin::users-permissions.user",
       ctx.state.user.id,
       {
         fields: ["id", "full_name", "nickname", "dm_name"],
-        data: ctx.request.body,
+        data,
       }
     );
 
