b96e8782c4e03821f1214a1320528af30c8650d7
Axel Raymundo <stephen.raymundo@gmail.com>
2024-03-27 21:46:59 +0800

    loc to place

--- src/api/game/content-types/game/lifecycles.js
+++ src/api/game/content-types/game/lifecycles.js
@@ -2,25 +2,61 @@ module.exports = {
   async beforeCreate(event) {
     const { data, where, select, populate } = event.params;
 
-    const place = await strapi.entityService.findOne(
-      "api::location.location",
-      data.place,
-      {}
-    );
+    if (data.updatedBy) {
+      if (JSON.stringify(data.place).includes("id")) {
+        //place was changed
+        if (data.place.connect.length > 0) {
+          const place = await strapi.entityService.findOne(
+            "api::location.location",
+            data.place.connect[0].id,
+            {}
+          );
 
-    event.params.data.location = place.name;
+          event.params.data.location = place.name;
+        } else {
+          event.params.data.location = "Not Available";
+        }
+      }
+    } else {
+      //normal
+      const place = await strapi.entityService.findOne(
+        "api::location.location",
+        data.place,
+        {}
+      );
+
+      event.params.data.location = place.name;
+    }
   },
 
   async beforeUpdate(event) {
     const { data, where, select, populate } = event.params;
 
-    const place = await strapi.entityService.findOne(
-      "api::location.location",
-      data.place,
-      {}
-    );
+    if (data.updatedBy) {
+      if (JSON.stringify(data.place).includes("id")) {
+        //place was changed
+        if (data.place.connect.length > 0) {
+          const place = await strapi.entityService.findOne(
+            "api::location.location",
+            data.place.connect[0].id,
+            {}
+          );
 
-    event.params.data.location = place.name;
+          event.params.data.location = place.name;
+        } else {
+          event.params.data.location = "Not Available";
+        }
+      }
+    } else {
+      //normal
+      const place = await strapi.entityService.findOne(
+        "api::location.location",
+        data.place,
+        {}
+      );
+
+      event.params.data.location = place.name;
+    }
   },
 
   afterUpdate(event) {
